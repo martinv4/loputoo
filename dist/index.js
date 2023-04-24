e409bc1362beea1dfbfa4d7bb854c40b991861ef
Klaus Martin Vare <klaus.martin.vare@tehik.ee>
2023-04-24 16:00:32 +0300

    variables, minor changes

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,21 @@
-var button = document.getElementById("testButton");
-var button2 = document.getElementById("button2");
+var pasteTextButton = document.getElementById("pasteTextButton");
 var langButton = document.getElementById("lang");
 var changeSpeaker = document.getElementById("appSpeaker");
 var changeTempo = document.getElementById("appSpeed");
 var tooltip = document.getElementById("tooltip-text");
 const target = document.getElementById("help");
 var audio = document.getElementById("audio");
-window.localStorage.setItem("preferDark", "EST");
+var currentLang = document.getElementById("lang");
+var spanText = document.getElementById("spanText");
+var speaker = document.getElementById("speakerSpan");
 
 async function getSelectionText() {
   document.body.style.cursor = 'wait';
   chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
 }, function(selection) {
-    testText2 = document.getElementById('testelement2').innerHTML;
-    document.getElementById("testelement2").innerHTML = selection[0];
-    console.log("test1")
+    currentText = document.getElementById('placeholderText').innerHTML;
+    document.getElementById("placeholderText").innerHTML = selection[0];
     //testida kas funktsioonide nestides alumine probleem paraneks
 });
 }
@@ -24,12 +24,7 @@ async function abcFunction(){
   await getSelectionText();
 
   appSpeed = document.getElementById('appSpeed').value;
-  console.log(appSpeed);
-
   appSpeaker = document.getElementById('appSpeaker').value;
-  console.log(appSpeaker)
-
-  console.log(testText2)
 
   const res = await fetch('https://api.tartunlp.ai/text-to-speech/v2', {
     method: 'POST',
@@ -37,7 +32,7 @@ async function abcFunction(){
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      text: testText2,
+      text: currentText,
       speaker: appSpeaker,
       speed: appSpeed})
   })
@@ -47,51 +42,41 @@ async function abcFunction(){
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      text: testText2,
+      text: currentText,
       speaker: appSpeaker,
       speed: appSpeed})
   }).then (res2 => res2.json());
   
-  console.log(res);
-  console.log(res2.duration_frames);
+  //lõikab algusest ja lõpust neurokõne api poolt tekitatud kasutud numbrid
   const frames = res2.duration_frames.slice(1, -1)
-  console.log(frames);
-  //const sum = framesClean.reduce((a,b)=>a+b);
-  //console.log(sum);
-  let out = frames
-  .join('#')                     // merge into a string
-  .split(/(?:^|#)(0)(?:#|$)/)    // split on X (but retain them)
-  .filter(Boolean)               // remove empty values
-  .map(v => v.split('#'));       // split on #
 
-console.log(out);
+  let durationAsString = frames
+   .join('#')                     // merge into a string
+   .split(/(?:^|#)(0)(?:#|$)/)    // split on X (but retain them)
+   .filter(Boolean)               // remove empty values
+   .map(v => v.split('#'));       // split on #
 
 //konverteerib subarrayd numbriteks
-function conv(out) {
+function conv(durationAsString) {
   // iterate over the array
-  return out.map(function(v) {
+  return durationAsString.map(function(v) {
     // if the element is an array call the function recursively
     // or parse the number and treat NaN as 0
     return Array.isArray(v) ? conv(v) : Number(v) || 0;
   })
 }
-outNumbers = conv(out);
-
-/* var numberArray = [];
-var arrayLength = out.length;
-for (var i = 0; i < arrayLength; i++)
-numberArray.push(parseInt(out[i]));
-console.log(numberArray);  */
+durationAsNumbers = conv(durationAsString);
 
 // liidab subarrayd
- var newOut = [];
-outNumbers.forEach(function(item) {
+ var durationFrames = [];
+durationAsNumbers.forEach(function(item) {
   item = item.reduce(function(a, b) {
     return a + b;
   });
-  newOut.push([item]);
+  durationFrames.push([item]);
 });
 
+//eemaldab 0 arrayd aka tühikud
 function removeSubArray(source, sub) {
   let i = source.length;
   while(i--) {
@@ -102,14 +87,12 @@ function removeSubArray(source, sub) {
 }
 
 const arr2 = [0];
+removeSubArray(durationFrames, arr2);
 
-removeSubArray(newOut, arr2);
-console.log(newOut);
-
-var words = $("#testelement2").text().split(" ");
-$("#testelement2").empty();
+var words = $("#placeholderText").text().split(" ");
+$("#placeholderText").empty();
 $.each(words, function(i, v) {
-    $("#testelement2").append($("<span class='testhighlight'>").text(v).append(" "));
+    $("#placeholderText").append($("<span class='highlighted'>").text(v).append(" "));
     
 });
 
@@ -119,23 +102,25 @@ $.each(words, function(i, v) {
 
   const wavFile = await res.blob();
   document.querySelector('audio').src = URL.createObjectURL(wavFile);
+
+  //bimodal / karaoke
   audio.onplay = function biModal(){
     var i=0;
     var changeText = function(){
-      $(".testhighlight:not(.blu):first")
+      $(".highlighted:not(.blu):first")
           .addClass("blu");
           i++;
-      setTimeout(changeText, newOut[i]/appSpeed*12);
-      console.log(newOut[i]);
+      setTimeout(changeText, durationFrames[i]/appSpeed*12);
+      console.log(durationFrames[i]);
      }
-     setTimeout(changeText, newOut[i]/appSpeed*12);
+     setTimeout(changeText, durationFrames[i]/appSpeed*12);
   }
   
   console.log(localStorage.toggled)
   document.body.style.cursor = 'default'
 }
 
-button2.addEventListener("click", function(){
+pasteTextButton.addEventListener("click", function(){
   getSelectionText();
   //pole just parim lahendus, ei tea miks esimene func varem lõpetab
   setTimeout(function (){
@@ -143,23 +128,21 @@ button2.addEventListener("click", function(){
   }, 10);
   abcFunction();
 })
-var x = document.getElementById("lang");
-var spanText = document.getElementById("spanText");
-var button = document.getElementById("button2");
-var speaker = document.getElementById("speaker");
 
+
+//UI language
 function changeLanguageENG() {
-    x.innerHTML = "🇬🇧";
+    currentLang.innerHTML = "🇬🇧";
     spanText.innerHTML = "Selected text:";
-    button.innerHTML = "Paste selection";
-    button.style.color = "#FFFFFF";
+    pasteTextButton.innerHTML = "Paste selection";
+    pasteTextButton.style.color = "#FFFFFF";
     speaker.innerHTML = "Speaker:"
   }
 function changeLanguageEST() {
-    x.innerHTML = "🇪🇪";
+    currentLang.innerHTML = "🇪🇪";
     spanText.innerHTML = "Valitud tekst:";
-    button.innerHTML = "Kleebi valik";
-    button.style.color = "#FFFFFF";
+    pasteTextButton.innerHTML = "Kleebi valik";
+    pasteTextButton.style.color = "#FFFFFF";
     speaker.innerHTML = "Kõneleja:"
   }
 
@@ -169,10 +152,11 @@ function changeLanguageEST() {
 
     if (localStorage.toggled != "EST" ) {
        localStorage.toggled = "EST";
+       changeLanguageEST();
     } else {
        localStorage.toggled = "ENG";
+       changeLanguageENG();
     }
-    console.log(localStorage.toggled)
  });
  window.addEventListener("load", (event) => {
   if (localStorage.toggled == "ENG"){
@@ -181,8 +165,6 @@ function changeLanguageEST() {
 });
 
 // et ei peaks Main nuppu vajutama peale sätete muutmist
-// ENABLE THIS
-//langButton.addEventListener("click", changeLanguage);
 changeSpeaker.addEventListener("change", abcFunction);
 changeTempo.addEventListener("change", abcFunction);
 
